933fe6d30ac52e5d84a160240f9787f2d70821fc
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-24 06:20:34 +0900

    clarify license

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,20 +4,10 @@
 #
 # Author:: Ryuzee <ryuzee@gmail.com>
 #
-# Copyright 2012, Ryutaro YOSHIBA 
+# Copyright 2013, Ryutaro YOSHIBA 
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in wrhiting, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
 
 case node[:platform]
 when "centos"
@@ -41,3 +31,5 @@ when "centos"
     provider Chef::Provider::Package::Rpm
   end
 end
+
+# vim: filetype=ruby.chef
